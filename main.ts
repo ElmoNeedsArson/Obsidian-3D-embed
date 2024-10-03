255e91fdf8d61fda0c6687336424247e59af3ecd
ElmoNeedsArson <jesse.strijker@gmail.com>
2024-10-04 00:56:50 +0200

    attempt1

--- main.ts
+++ main.ts
@@ -1,134 +1,87 @@
 import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import * as THREE from 'three';
 
-// Remember to rename these classes and interfaces!
-
-interface MyPluginSettings {
-	mySetting: string;
-}
-
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
-}
-
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
-
+export default class ThreeJSPlugin extends Plugin {
 	async onload() {
-		await this.loadSettings();
-
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
 		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
+			id: 'insert-threejs-scene',
+			name: 'Insert Three.js Scene',
+			callback: () => this.insertThreeJSScene()
 		});
-		// This adds an editor command that can perform some operation on the current editor instance
+
 		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
+			id: "3DModel",
+			name: "3DModel",
+			editorCallback: (editor: Editor) => {
+				const selection = editor.getSelection();
+				let firstLine = "```dataviewjs\n"
+				let secondLine = 'await dv.view("threejsinobsidian",{\n'
+				let thirdLine = 'el: dv,\n name: "' + selection + '",\n rotationX: 0,\n rotationY: 0,\n rotationZ: 0,\n scale: 0.5,\n'
+				let lastLine = '});\n```\n'
+				let content = firstLine + secondLine + thirdLine + lastLine
+				editor.replaceSelection(content);
+			},
 		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
+	}
 
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
+	insertThreeJSScene() {
+		console.log("1")
+		const activeView = this.app.workspace.getActiveViewOfType(MarkdownView);
+		if (!activeView) {
+			return;
+		}
+		console.log("2")
 
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
+		const editor = activeView.editor;
+		const sceneID = `threejs-scene-${Date.now()}`;
+		console.log("3")
 
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
+		// Insert a placeholder for the Three.js scene
+		editor.replaceSelection(`<div id="${sceneID}" class="threejs-scene" style="width: 100%; height: 400px;"></div>\n`);
+		console.log("4")
+		// Refresh the preview to show the changes (re-render the view)
+		// activeView.previewMode.rerender();
 
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+		console.log("5")
+
+		// Listen for the markdown render event to inject the Three.js scene into preview
+		//this.registerMarkdownPostProcessor((element, context) => {
+		// Find the div with the specific class (that we inserted in the markdown)
+		const container = document.querySelector('.threejs-scene') as HTMLElement;
+		if (container) {
+			this.loadThreeJSScene(container);
+		}
+		//});
+
+	}
+
+	loadThreeJSScene(container: HTMLElement) {
+		console.log("Loading threejs scene")
+		const scene = new THREE.Scene();
+		const camera = new THREE.PerspectiveCamera(75, container.offsetWidth / 400, 0.1, 1000);
+		const renderer = new THREE.WebGLRenderer();
+
+		renderer.setSize(container.offsetWidth, 400);
+		container.appendChild(renderer.domElement);
+
+		const geometry = new THREE.BoxGeometry();
+		const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+		const cube = new THREE.Mesh(geometry, material);
+		scene.add(cube);
+
+		camera.position.z = 5;
+
+		function animate() {
+			requestAnimationFrame(animate);
+			cube.rotation.x += 0.01;
+			cube.rotation.y += 0.01;
+			renderer.render(scene, camera);
+		}
+
+		animate();
 	}
 
 	onunload() {
-
-	}
-
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
-
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
-}
-
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
-
-	display(): void {
-		const {containerEl} = this;
-
-		containerEl.empty();
-
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
+		console.log('ThreeJS plugin unloaded');
 	}
 }

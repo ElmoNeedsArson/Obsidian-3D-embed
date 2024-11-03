e3d4d39dedfcbda9d06da6e60e510918c5efb2fe
ElmoNeedsArson <jesse.strijker@gmail.com>
2024-11-03 18:12:32 +0100

    setting update

--- main.ts
+++ main.ts
@@ -7,10 +7,14 @@ import { STLLoader } from 'three/examples/jsm/loaders/STLLoader.js';
 
 interface ThreeDEmbedSettings {
     standardColor: string;
+    standardScale: number;
+    standardEmbedHeight: number;
 }
 
 const DEFAULT_SETTINGS: Partial<ThreeDEmbedSettings> = {
-    standardColor: "#ADD8E6"
+    standardColor: "#ADD8E6",
+    standardScale: 0.5,
+    standardEmbedHeight: 300,
 };
 
 export default class ThreeJSPlugin extends Plugin {
@@ -28,16 +32,17 @@ export default class ThreeJSPlugin extends Plugin {
         await this.loadSettings();
         this.addSettingTab(new ThreeDSettingsTab(this.app, this));
 
+        //adds a code block that instantly adds the 3D scene to your note
         this.addCommand({
             id: "3DModel",
             name: "3DModel",
-            //hotkeys: [{ modifiers: [this.settings.keyOne], key: this.settings.keyTwo }],
             editorCallback: (editor: Editor) => {
                 let selection = editor.getSelection();
+
+                //gets the embed on the line to get the 3D model name in the vault
                 if (selection == "") {
                     const lineNumber = editor.getCursor().line
                     const searchQuery = editor.getLine(lineNumber).trim()
-                    //console.log("Sq: " + searchQuery)
 
                     function mySubString(str: string) {
                         let newStr;
@@ -47,23 +52,32 @@ export default class ThreeJSPlugin extends Plugin {
                     let newStr1 = mySubString(searchQuery)
                     let newStr2 = mySubString(newStr1)
                     selection = newStr2;
-                    //console.log("Newstring: " + newStr2)
                 }
 
-                //const searchQuery = doc.getLine(curLineNum).trim()
-                let firstLine = "\n```3D\n"
-                let contents = `{\n"name": "` + selection + `",\n"rotationX": 0, "rotationY": 0, "rotationZ": 0, \n"scale": 0.5, \n"AutorotateX": 0, "AutorotateY": 0.001, "AutorotateZ": 0, \n"colorHexString": "` + this.settings.standardColor.replace(/#/g, "") + `", \n"positionX": 0, "positionY": 0, "positionZ": 0\n}\n`
-                let lastLine = '\n```\n'
-                let content = firstLine + contents + lastLine
-                editor.replaceSelection(content);
+                //If a path to the selection can be found the model can be displayed, otherwise display a warning
+                const modelPath = this.getModelPath(selection)
+                if (!modelPath) { new Notice("This model cannot be found", 5000); }
+                else if (modelPath) {
+                    let codeBlockType = "\n```3D\n{"
+                    let name = `\n"name": "` + selection + `"`
+                    let rotation = `,\n"rotationX": 0, "rotationY": 0, "rotationZ": 0`
+                    let autoRotate = `,\n"AutorotateX": 0, "AutorotateY": 0.001, "AutorotateZ": 0`
+                    let position = `,\n"positionX": 0, "positionY": 0, "positionZ": 0`
+                    let scale = `,\n"scale": "` + this.settings.standardScale + `"`
+                    let color = `,\n"colorHexString": "` + this.settings.standardColor.replace(/#/g, "") + `"`
+                    let codeBlockClosing = '\n}\n```\n'
+
+                    let content = codeBlockType + name + rotation + autoRotate + position + scale + color + codeBlockClosing
+                    editor.replaceSelection(content);
+                }
             },
         });
 
         this.registerMarkdownCodeBlockProcessor('3D', (source, el, ctx) => {
-            //console.log((ctx as any).containerEl);
             try {
-                console.log(el)
+                console.log(source)
                 const parsedData = JSON.parse(source);
+                console.log(parsedData)
                 const modelPath = this.getModelPath(parsedData.name);
                 if (!modelPath) throw new Error("Model path not found");
                 const containerEl = (ctx as any).containerEl;
@@ -85,11 +99,11 @@ export default class ThreeJSPlugin extends Plugin {
         const scene = new THREE.Scene();
         scene.background = new THREE.Color(`#${config.colorHexString || this.settings.standardColor.replace(/#/g, "")}`);
 
-        const camera = new THREE.PerspectiveCamera(75, width / 300, 0.1, 1000);
+        const camera = new THREE.PerspectiveCamera(75, width / this.settings.standardEmbedHeight, 0.1, 1000);
         camera.position.z = 10;
 
         const renderer = new THREE.WebGLRenderer();
-        renderer.setSize(width, 300);
+        renderer.setSize(width, this.settings.standardEmbedHeight);
         el.appendChild(renderer.domElement);
         console.log("Renderer added")
 
@@ -118,8 +132,8 @@ export default class ThreeJSPlugin extends Plugin {
             const firstChild = containerEl.firstChild as HTMLElement; // Cast to HTMLElement
             if (firstChild) {
                 const newWidth = firstChild.clientWidth; // Now TypeScript recognizes clientWidth
-                renderer.setSize(newWidth, 300);
-                camera.aspect = newWidth / 300;
+                renderer.setSize(newWidth, this.settings.standardEmbedHeight);
+                camera.aspect = newWidth / this.settings.standardEmbedHeight;
                 camera.updateProjectionMatrix();
             }
         };
@@ -137,13 +151,9 @@ export default class ThreeJSPlugin extends Plugin {
         const animate = () => {
             requestAnimationFrame(animate);
 
-            /*if(config.autoRotate){
-                if(ThreeDmodel){
-                ThreeDmodel.rotation.y += 0.001;}
-            }*/
             controls.update();
             renderer.render(scene, camera);
-            
+
             if (ThreeDmodel) {
                 ThreeDmodel.rotation.y += config.AutorotateY;
                 ThreeDmodel.rotation.x += config.AutorotateX;
@@ -216,5 +226,33 @@ class ThreeDSettingsTab extends PluginSettingTab {
                         await this.plugin.saveSettings();
                     })
             );
+
+        new Setting(containerEl)
+            .setName('Standard Scale of Model')
+            .setDesc('Default Size of 3D models in scene (non whole numbers should be seperated by dot, not comma)')
+            .addText(text =>
+                text
+                    .setValue(this.plugin.settings.standardScale.toString())
+                    .onChange(async (value) => {
+                        const numValue = parseFloat(value)
+                        this.plugin.settings.standardScale = numValue;
+                        await this.plugin.saveSettings();
+                    })
+
+            )
+
+        new Setting(containerEl)
+            .setName('Standard Height')
+            .setDesc('Default height of a 3D model embed in your note (in pixels)')
+            .addText(text =>
+                text
+                    .setValue(this.plugin.settings.standardEmbedHeight.toString())
+                    .onChange(async (value) => {
+                        const numValue = parseFloat(value)
+                        this.plugin.settings.standardEmbedHeight = numValue;
+                        await this.plugin.saveSettings();
+                    })
+
+            )
     }
 }
